52562d41af8486134a230a4cb3cd084b39acbe2c
yu huo <huo.125@osu.edu>
2022-09-20 18:47:55 -0400

    actions.py

--- project/actions/actions.py
+++ project/actions/actions.py
@@ -7,10 +7,31 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
+from rasa_sdk import Action
+from rasa_sdk.events import SlotSet
+import requests
+
+class FetchProfileAction(Action):
+
+    def name(self): 
+        return "fetch_profile" 
+
+    def run(self, dispatcher, tracker, domain): 
+        url = "http://myprofileurl.com" 
+        data = requests.get(url).json 
+        return [SlotSet("account_type", data["account_type"])]
+    
+    def extract_class_title(self, dispatcher, tracker, domain):
+        class_name = tracker.get_slot('class')
+        url = "https://content.osu.edu/v2/classes/search?q=" + class_name
+        data = requests.get(url).json
+        # find "shortDescription tuple"
+        return []
+    
+    
+
+
 # from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
 #
 #
 # class ActionHelloWorld(Action):
