5ae432cabfdc1b480330a187b24e7e7cf0be7e75
cai882 <cai.882@buckeyemail.osu.edu>
2022-09-27 17:45:56 -0400

    add more actions

--- project/actions/actions.py
+++ project/actions/actions.py
@@ -7,6 +7,7 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
+import re
 from rasa_sdk import Action
 from rasa_sdk.events import SlotSet
 import requests
@@ -28,6 +29,27 @@ class FetchProfileAction(Action):
         print(url)
         data = requests.get(url).json()
         outout = data['data']['courses'][0]["course"]["shortDescription"]
+        return  outout
+
+    def extract_class_term(self, dispatcher, tracker, domain):
+        class_name = tracker.get_slot('class')
+        url = "https://content.osu.edu/v2/classes/search?q=" + class_name
+        data = requests.get(url).json()
+        outout = data['data']['courses'][0]["course"]["term"]
+        return  outout
+
+    def extract_class_building(self, dispatcher, tracker, domain):
+        class_name = tracker.get_slot('class')
+        url = "https://content.osu.edu/v2/classes/search?q=" + class_name
+        data = requests.get(url).json()
+        outout = data['data']['courses'][0]['sections'][0]['meetings'][0]['buildingDescription']
+        return  outout
+
+    def extract_class_campus(self, dispatcher, tracker, domain):
+        class_name = tracker.get_slot('class')
+        url = "https://content.osu.edu/v2/classes/search?q=" + class_name
+        data = requests.get(url).json()
+        outout = data['data']['courses'][0]['sections'][0]['campus']
         return  outout
     
 
