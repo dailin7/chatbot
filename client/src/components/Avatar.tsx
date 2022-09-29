fd4e53acec15ef88c418fa7754b53726541ba12b
Yunqi Zhang <zhang.8678@osu.edu>
2022-09-29 14:59:32 -0400

    chatbot component overlap

---
+++ client/src/components/Avatar.tsx
@@ -0,0 +1,35 @@
+
+import React, { useState } from "react";
+
+import { styles } from './styles'
+
+import SupportWindow from "./SupportWindow";
+
+const Avatar = (props: any) => {
+    const [hovered, setHovered] = useState(false)
+
+    return (
+        <div style={props.style}>
+            {hovered && <SupportWindow />}
+            
+            <div 
+            
+                //onMouseEnter={() => setHovered(true)}
+                //onMouseLeave={() => setHovered(false)} 
+                onClick={() => setHovered(true)}
+                onBlur={() => setHovered(false)}
+                style={{
+                    ...styles.chatWithMeButton,
+                    ...{ border: hovered ? '1px solid #666666' : '4px solid #BB0000'  }
+        
+                    
+                }}
+                
+
+            />
+           
+            
+        </div>
+    )
+}
+export default Avatar
