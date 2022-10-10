4c21ed6ed5db896c6ba3f4df8fc6a0db148dbeaa
Ray Chung <raychungno1@gmail.com>
2022-10-09 21:20:51 -0400

    modified course page to add accordion

--- client/src/components/CourseDetails.tsx
+++ client/src/components/CourseDetails.tsx
@@ -1,8 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Accordion, AccordionDetails, AccordionSummary } from "@mui/material";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import { useParams } from "react-router-dom";
+
 import CourseTag from "./CourseTag";
 import FeaturedLink from "./FeaturedLink";
+
 import { sampleCourse } from "./sampleCourse";
 
 const details = sampleCourse.course;
@@ -10,8 +14,9 @@ const sections = sampleCourse.sections;
 const section = sections[0];
 
 const CourseDetails = () => {
-  const { courseId } = useParams();
-  console.log(section);
+  // const { courseId } = useParams();
+  // console.log(section);
+
   return (
     <div className="max-w-[1536px] w-[90%] mx-auto mt-6">
       <Link to="/">
@@ -41,61 +46,97 @@ const CourseDetails = () => {
       </p>
       <div className="relative z-10">
         {sections.map((section) => (
-          <div className="bg-white shadow-md p-6 w-full flex mb-4 gap-4 lg:items-center">
-            <p className="w-[100px] text-lg font-bold">{section.component}</p>
-            {section.meetings.map(
-              ({
-                monday,
-                tuesday,
-                wednesday,
-                thursday,
-                friday,
-                startTime,
-                endTime,
-                buildingDescription,
-                instructors,
-              }) => (
-                <div className="w-full flex flex-col lg:flex-row lg:items-center justify-between gap-1 lg:gap-4">
-                  <div className="lg:w-1/2 xl:w-1/3 flex flex-col xs:flex-row gap-2 xs:items-center">
-                    <div className="flex gap-2 items-center">
-                      {monday && (
-                        <CourseTag className="w-9 h-9 flex items-center justify-center text-base">
-                          M
-                        </CourseTag>
-                      )}
-                      {tuesday && (
-                        <CourseTag className="w-9 h-9 flex items-center justify-center text-base">
-                          Tu
-                        </CourseTag>
-                      )}
-                      {wednesday && (
-                        <CourseTag className="w-9 h-9 flex items-center justify-center text-base">
-                          W
-                        </CourseTag>
-                      )}
-                      {thursday && (
-                        <CourseTag className="w-9 h-9 flex items-center justify-center text-base">
-                          Th
-                        </CourseTag>
-                      )}
-                      {friday && (
-                        <CourseTag className="w-9 h-9 flex items-center justify-center text-base">
-                          F
-                        </CourseTag>
-                      )}
-                    </div>
-                    <div className="xs:border-l-2 border-gray-200 xs:pl-2">
-                      {startTime} - {endTime}
-                    </div>
-                  </div>
-                  <div className="lg:w-1/4 xl:w-1/3">{buildingDescription}</div>
-                  <div className="lg:w-1/4 xl:w-1/3">
-                    {instructors[0].displayName}
-                  </div>
+          <Accordion square={true}>
+            <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+              <div className="w-full flex gap-4 lg:items-center">
+                <div>
+                  <p className="font-bold text-sm">Type</p>
+                  <p className="w-[100px]">{section.component}</p>
                 </div>
-              )
-            )}
-          </div>
+                {section.meetings.map(
+                  ({
+                    monday,
+                    tuesday,
+                    wednesday,
+                    thursday,
+                    friday,
+                    startTime,
+                    endTime,
+                    buildingDescription,
+                    instructors,
+                  }) => (
+                    <div className="w-full flex md:flex-col gap-4 md:gap-0">
+                      <div className="flex flex-col md:flex-row md:items-center justify-between gap-1 lg:gap-4">
+                        <div className="md:h-auto md:w-1/2 xl:w-1/3 ">
+                          <p className="font-bold text-sm">Time</p>
+                          <div className="flex flex-col xs:flex-row gap-2 xs:items-center">
+                            <div className="flex gap-2 items-center">
+                              {monday && (
+                                <CourseTag className="w-6 h-6 flex items-center justify-center text-base">
+                                  M
+                                </CourseTag>
+                              )}
+                              {tuesday && (
+                                <CourseTag className="w-6 h-6 flex items-center justify-center text-base">
+                                  Tu
+                                </CourseTag>
+                              )}
+                              {wednesday && (
+                                <CourseTag className="w-6 h-6 flex items-center justify-center text-base">
+                                  W
+                                </CourseTag>
+                              )}
+                              {thursday && (
+                                <CourseTag className="w-6 h-6 flex items-center justify-center text-base">
+                                  Th
+                                </CourseTag>
+                              )}
+                              {friday && (
+                                <CourseTag className="w-6 h-6 flex items-center justify-center text-base">
+                                  F
+                                </CourseTag>
+                              )}
+                            </div>
+                            <div className="xs:border-l-2 border-gray-200 xs:pl-2">
+                              {startTime} - {endTime}
+                            </div>
+                          </div>
+                        </div>
+                        <div className="md:w-1/4 xl:w-1/3">
+                          <p className="font-bold text-sm">Location</p>
+                          {buildingDescription}
+                        </div>
+                        <div className="md:w-1/4 xl:w-1/3">
+                          <p className="font-bold text-sm">Instructor</p>
+                          {instructors[0].displayName}
+                        </div>
+                      </div>
+                    </div>
+                  )
+                )}
+              </div>
+            </AccordionSummary>
+            <AccordionDetails>
+              <p>
+                <span className="font-bold">Course ID: </span>
+                {section.courseId}
+              </p>
+              <p>
+                <span className="font-bold">Section: </span>
+                {section.section}
+              </p>
+              <p>
+                <span className="font-bold">Instruction Mode: </span>
+                {section.instructionMode}
+              </p>
+              {section.attributes.map(({ name, description }) => (
+                <p>
+                  <span className="font-bold">{name}: </span>
+                  {description}
+                </p>
+              ))}
+            </AccordionDetails>
+          </Accordion>
         ))}
       </div>
       <div className="h-12" />
