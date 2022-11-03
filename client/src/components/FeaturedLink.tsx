8439a0fad23296416cdad03fae0dfb5aef0dbd85
Yunqi Zhang <zhang.8678@osu.edu>
2022-11-03 17:36:38 -0400

    update

--- client/src/components/FeaturedLink.tsx
+++ client/src/components/FeaturedLink.tsx
@@ -4,9 +4,10 @@ interface IProps {
   children?: string | JSX.Element | JSX.Element[];
   className?: string;
   direction?: "left" | "right";
+  onClick?: React.MouseEventHandler<any>;
 }
 
-const FeaturedLink = ({ children, className, direction = "right" }: IProps) => {
+const FeaturedLink = ({ children, className,onClick, direction = "right" }: IProps) => {
   const after =
     direction === "right"
       ? "after:-right-4 after:content-['>'] hover:after:translate-x-1"
@@ -15,9 +16,11 @@ const FeaturedLink = ({ children, className, direction = "right" }: IProps) => {
   return (
     <div
       className={`relative font-semibold w-fit hover:underline after:absolute after:top-0 after:transition-transform after:text-scarlet ${after} ${className}`}
+      onClick={onClick}
     >
       {children}
     </div>
+    
   );
 };
 
